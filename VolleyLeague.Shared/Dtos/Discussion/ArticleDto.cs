3b98a7326a44775ddc274c631f67d80948d35d45
lempartmar <lempartmar@gmail.com>
2024-06-24 15:44:13 +0200

    AddArticle - additional validation

--- VolleyLeague.Shared/Dtos/Discussion/ArticleDto.cs
+++ VolleyLeague.Shared/Dtos/Discussion/ArticleDto.cs
@@ -1,15 +1,20 @@
-﻿namespace VolleyLeague.Shared.Dtos.Discussion
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VolleyLeague.Shared.Dtos.Discussion
 {
     public partial class ArticleDto
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Tytuł jest wymagany")]
+        public string Title { get; set; } = null!;
+
+        [Required(ErrorMessage = "Zawartość jest wymagana")]
         public string Content { get; set; } = null!;
 
         public DateTime CreationDate { get; set; }
 
-        public string Title { get; set; } = null!;
-
+        [Required(ErrorMessage = "Zdjęcie jest wymagane")]
         public byte[] Image { get; set; } = null!;
 
         //public virtual AuthorInfoDto Author { get; set; } = null!;
