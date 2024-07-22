ea6ab2ce33d3c6a307a0bd1b72092b9fdfc6a4fc
lempartmar <lempartmar@gmail.com>
2024-07-22 11:03:33 +0200

    NavMenu - add swiper function to move between two pages

---
+++ VolleyLeague.Client.Blazor/wwwroot/assets/js/swiperInit.js
@@ -0,0 +1,13 @@
+window.initializeSwiper = () => {
+    new Swiper('.swiper.bannerSlide2', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+};
