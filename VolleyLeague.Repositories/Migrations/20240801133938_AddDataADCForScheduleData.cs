372bbb9c11841369555883c73cad62aaefac1dbd
lempartmar <lempartmar@gmail.com>
2024-08-01 15:50:20 +0200

    Edit ADC - change in round update

---
+++ VolleyLeague.Repositories/Migrations/20240801133938_AddDataADCForScheduleData.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VolleyLeague.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDataADCForScheduleData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AdminDefinedCodes",
+                columns: new[] { "Key", "Value", "CreatedDate", "ModifiedDate" },
+                values: new object[] { "leagueId-for-schedule", "1", DateTime.UtcNow, DateTime.UtcNow }
+            );
+            migrationBuilder.InsertData(
+                table: "AdminDefinedCodes",
+                columns: new[] { "Key", "Value", "CreatedDate", "ModifiedDate" },
+                values: new object[] { "seasonId-for-schedule", "37", DateTime.UtcNow, DateTime.UtcNow }
+            );
+            migrationBuilder.InsertData(
+                table: "AdminDefinedCodes",
+                columns: new[] { "Key", "Value", "CreatedDate", "ModifiedDate" },
+                values: new object[] { "roundId-for-schedule", "1237", DateTime.UtcNow, DateTime.UtcNow }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
