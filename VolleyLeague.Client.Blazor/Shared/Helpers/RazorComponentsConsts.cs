5b21927d35cf9c3a3e4c1d14d460b1037acb1af5
lempartmar <lempartmar@gmail.com>
2024-08-01 14:23:24 +0200

    MainPage - results from ADC table

---
+++ VolleyLeague.Client.Blazor/Shared/Helpers/RazorComponentsConsts.cs
@@ -0,0 +1,8 @@
+﻿namespace VolleyLeague.Client.Blazor.Shared.Helpers
+{
+    public static class RazorComponentsConsts
+    {
+        public static string BLSLeagueName = "1. Liga BLS";
+        public static string BLSKLeagueName = "1. Liga BLSK";
+    }
+}
