14f4152572e37c3bf2d47e1fd719ade7249de68e
lempartmar <lempartmar@gmail.com>
2024-08-01 15:14:06 +0200

    ADC - add link to YT channel

---
+++ VolleyLeague.Repositories/Migrations/20240801124809_AddADCYoutubeLinkData.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VolleyLeague.Repositories.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddADCYoutubeLinkData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AdminDefinedCodes",
+                columns: new[] { "Key", "Value", "CreatedDate", "ModifiedDate" },
+                values: new object[] { "youtube-link1", "https://www.youtube.com/embed/DID8UK3gLj8", DateTime.UtcNow, DateTime.UtcNow });
+            migrationBuilder.InsertData(
+                table: "AdminDefinedCodes",
+                columns: new[] { "Key", "Value", "CreatedDate", "ModifiedDate" },
+                values: new object[] { "youtube-link2", "https://www.youtube.com/embed/ZgunK7Wgfvc", DateTime.UtcNow, DateTime.UtcNow });
+            migrationBuilder.InsertData(
+                table: "AdminDefinedCodes",
+                columns: new[] { "Key", "Value", "CreatedDate", "ModifiedDate" },
+                values: new object[] { "youtube-link3", "https://www.youtube.com/embed/imtHlApQ-EU", DateTime.UtcNow, DateTime.UtcNow });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
