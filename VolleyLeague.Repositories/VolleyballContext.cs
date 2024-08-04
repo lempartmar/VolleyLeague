6146171f4d67afaf698f216c4f62c5ffcdc61946
lempartmar <lempartmar@gmail.com>
2024-08-04 15:00:18 +0200

    Added connection string from webio

--- VolleyLeague.Repositories/VolleyballContext.cs
+++ VolleyLeague.Repositories/VolleyballContext.cs
@@ -9,14 +9,14 @@ namespace VolleyLeague.Repositories
 
         public VolleyballContext() : base()
         {
-            _connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=tomasz1_VolleyballDatabase;Integrated Security=True;TrustServerCertificate=true;Persist Security Info=False";
+            _connectionString = "Data Source=mssql4.webio.pl,2401;Initial Catalog=tomasz1_tomasz1_VolleyballDatabase_test;Persist Security Info=True;User ID=tomasz1_student;Password=SDK#$$O5kfvas21;TrustServerCertificate=true";
 
         }
 
         public VolleyballContext(DbContextOptions<VolleyballContext> options)
             : base(options)
         {
-            _connectionString = "Data Source=localhost\\SQLEXPRESS;Initial Catalog=tomasz1_VolleyballDatabase;Integrated Security=True;TrustServerCertificate=true;Persist Security Info=False";
+            _connectionString = "Data Source=mssql4.webio.pl,2401;Initial Catalog=tomasz1_tomasz1_VolleyballDatabase_test;Persist Security Info=True;User ID=tomasz1_student;Password=SDK#$$O5kfvas21;TrustServerCertificate=true";
         }
 
         public static string SchemaName = "tomasz1_voladmin";
