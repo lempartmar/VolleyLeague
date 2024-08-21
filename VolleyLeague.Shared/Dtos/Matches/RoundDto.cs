b1cdf332f91fb0d794f67a96e20ae65cbe648bfc
lempartmar <lempartmar@gmail.com>
2024-08-21 20:47:46 +0200

    Handle season - changes in round and season

--- VolleyLeague.Shared/Dtos/Matches/RoundDto.cs
+++ VolleyLeague.Shared/Dtos/Matches/RoundDto.cs
@@ -1,11 +1,16 @@
-﻿using VolleyLeague.Entities.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using VolleyLeague.Entities.Models;
 
 namespace VolleyLeague.Shared.Dtos.Matches
 {
     public class RoundDto
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nazwa kolejki jest wymagana")]
+        [StringLength(100, ErrorMessage = "Nazwa kolejki nie może być dłuższa niż 100 znaków")]
         public string Name { get; set; } = null!;
+
         public int SeasonId { get; set; }
 
         public static explicit operator RoundDto(Round round)
--- VolleyLeague.Shared/Dtos/Teams/SeasonDto.cs
+++ VolleyLeague.Shared/Dtos/Teams/SeasonDto.cs
@@ -1,10 +1,14 @@
-﻿using VolleyLeague.Entities.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using VolleyLeague.Entities.Models;
 
 namespace VolleyLeague.Shared.Dtos.Teams
 {
     public class SeasonDto
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nazwa sezonu jest wymagana")]
+        [StringLength(100, ErrorMessage = "Nazwa sezonu nie może przekraczać 100 znaków")]
         public string Name { get; set; }
 
         public static explicit operator SeasonDto(Season season)
